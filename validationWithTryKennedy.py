666c1b70bef3eb95dd2d4399e0d5ca6e7fe05a16
DILLORG <tepidslurpee@protonmail.com>
2021-02-10 00:59:57 +0000

    Add files via upload

---
+++ validationWithTryKennedy.py
@@ -0,0 +1,34 @@
+"""
+Program: validationWithTryKennedy.py
+Author: Dylan Kennedy
+Last date modified: 02/09/2021
+
+The purpose of this program is
+to calculate the student's average score.
+"""
+
+NUM_TEST = 3
+
+def average(scores):
+    totalScore = 0.0
+    for score in scores:
+        totalScore += score
+    return float(totalScore / NUM_TEST)
+
+
+
+scores = []
+count = 0
+while count < NUM_TEST:
+
+    try:
+        test = float(input(f"Please enter score for test {count +1}> "))
+        scores.append(test)
+        count+=1
+    except ValueError:
+        print("Invalid score")
+
+
+averageScore = average(scores)
+
+print(f"The student's average score is {averageScore:.1f} ")
